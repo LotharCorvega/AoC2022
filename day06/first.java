237100e94210cd1ec6f290615dc4bfa402b4279f
Lothar <scherer.peer@hotmail.com>
2022-12-06 15:31:19 +0100

    day6

--- day06/first.java
+++ day06/first.java
@@ -2,16 +2,40 @@ package day06;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class first {
 	public static void main(String[] args) throws FileNotFoundException {
 		Scanner scanner = new Scanner(new File("day06/input.txt"));
-		ArrayList<String> input = new ArrayList<String>();
+		String message = scanner.nextLine();
+		scanner.close();
 
-		while (scanner.hasNextLine()) {
-			input.add(scanner.nextLine());
+		int markerSize = 4;
+		int count = 0;
+
+		for (int i = markerSize; i <= message.length(); i++) {
+			if (isMarker(message.substring(i - markerSize, i))) {
+				count += i;
+				break;
+			}
 		}
+
+		System.out.println(count);
+	}
+
+	public static boolean isMarker(String s) {
+		for (int i = 0; i < s.length(); i++) {
+			char c0 = s.charAt(i);
+
+			for (int j = 0; j < s.length(); j++) {
+				char c1 = s.charAt(j);
+
+				if (j != i && c0 == c1) {
+					return false;
+				}
+			}
+		}
+
+		return true;
 	}
 }

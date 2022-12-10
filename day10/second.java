655b567d4a1ce50a048701040a8d067104423314
Lothar <scherer.peer@hotmail.com>
2022-12-10 11:33:57 +0100

    day 10

---
+++ day10/second.java
@@ -0,0 +1,52 @@
+package day10;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class second {
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner scanner = new Scanner(new File("day10/input.txt"));
+		ArrayList<String> instructions = new ArrayList<String>();
+
+		while (scanner.hasNextLine()) {
+			instructions.add(scanner.nextLine());
+		}
+
+		scanner.close();
+
+		int n = instructions.size();
+		int[] register = new int[2 * n];
+
+		register[0] = 1;
+		int cycle = 1;
+
+		for (int i = 0; i < n; i++) {
+			if (instructions.get(i).equals("noop")) {
+				register[cycle] = register[cycle - 1];
+			} else {
+				int value = Integer.parseInt(instructions.get(i).substring(5));
+
+				register[cycle + 0] = register[cycle - 1];
+				register[cycle + 1] = register[cycle - 1] + value;
+				cycle++;
+			}
+			cycle++;
+		}
+
+		for (int y = 0; y < 6; y++) {
+			for (int x = 0; x < 40; x++) {
+				int pos = x + 40 * y;
+
+				if (Math.abs(register[pos] - x) < 2) {
+					System.out.print("#");
+				} else {
+					System.out.print(".");
+				}
+
+			}
+			System.out.println();
+		}
+	}
+}
